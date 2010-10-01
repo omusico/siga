315ffce6a4049dbb11a811f2646585e50e68c782
Javier Estévez <jestevez@cartolab.es>
2010-10-01 12:23:55 +0200

    Fixes on license

--- src/es/udc/cartolab/gvsig/elle/gui/wizard/save/SaveMapWizardComponent.java
+++ src/es/udc/cartolab/gvsig/elle/gui/wizard/save/SaveMapWizardComponent.java
@@ -1,20 +1,19 @@
 /*
- * Copyright (c) 2010. Cartolab (Universidade da Coruña)
+ * Copyright (c) 2010. CartoLab, Universidad de A Coruña
  * 
- * This file is part of extELLE
+ * This file is part of ELLE
  * 
- * extELLE is free software: you can redistribute it and/or modify it under the terms
+ * ELLE is free software: you can redistribute it and/or modify it under the terms
  * of the GNU General Public License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
+ * version 3 of the License, or any later version.
  * 
- * extELLE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * ELLE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License along with extELLE.
+ * You should have received a copy of the GNU General Public License along with ELLE.
  * If not, see <http://www.gnu.org/licenses/>.
- */
-
+*/
 package es.udc.cartolab.gvsig.elle.gui.wizard.save;
 
 import java.awt.event.ActionEvent;
