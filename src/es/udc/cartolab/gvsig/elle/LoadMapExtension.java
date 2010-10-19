d63af5b4b84c8ba9d5615ed557df56989e6472b2
Javier Estévez <jestevez@cartolab.es>
2010-10-19 14:07:27 +0200

    New fancy button icons

--- src/es/udc/cartolab/gvsig/elle/LoadMapExtension.java
+++ src/es/udc/cartolab/gvsig/elle/LoadMapExtension.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (c) 2010. CartoLab, Universidad de A Coruña
- * 
+ *
  * This file is part of ELLE
- * 
+ *
  * ELLE is free software: you can redistribute it and/or modify it under the terms
  * of the GNU General Public License as published by the Free Software Foundation, either
  * version 3 of the License, or any later version.
- * 
+ *
  * ELLE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with ELLE.
  * If not, see <http://www.gnu.org/licenses/>.
 */
@@ -47,6 +47,12 @@ public class LoadMapExtension extends Extension implements IPreferenceExtension 
 
 		//		carga la pestaña en añadir capa
 		AddLayer.addWizard(ElleWizard.class);
+
+		//icons
+		PluginServices.getIconTheme().registerDefault(
+				"load-map",
+				this.getClass().getClassLoader().getResource("images/mapacargar.png")
+			);
 	}
 
 	public boolean isEnabled() {
--- src/es/udc/cartolab/gvsig/elle/SaveAllLegendsExtension.java
+++ src/es/udc/cartolab/gvsig/elle/SaveAllLegendsExtension.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (c) 2010. CartoLab, Universidad de A Coruña
- * 
+ *
  * This file is part of ELLE
- * 
+ *
  * ELLE is free software: you can redistribute it and/or modify it under the terms
  * of the GNU General Public License as published by the Free Software Foundation, either
  * version 3 of the License, or any later version.
- * 
+ *
  * ELLE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with ELLE.
  * If not, see <http://www.gnu.org/licenses/>.
 */
@@ -45,7 +45,10 @@ public class SaveAllLegendsExtension extends Extension {
 	}
 
 	public void initialize() {
-
+		PluginServices.getIconTheme().registerDefault(
+				"save-all-legends",
+				this.getClass().getClassLoader().getResource("images/leyguardar.png")
+			);
 	}
 
 	public boolean isEnabled() {
