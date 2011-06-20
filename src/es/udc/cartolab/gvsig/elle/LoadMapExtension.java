38bd8131a805e8bec47790b1816c8d52f6fbe508
psanxiao <psanxiao@icarto.es>
2011-06-20 19:00:50 +0200

    Creating view when no ones exists on load map

--- src/es/udc/cartolab/gvsig/elle/LoadMapExtension.java
+++ src/es/udc/cartolab/gvsig/elle/LoadMapExtension.java
@@ -13,7 +13,7 @@
  *
  * You should have received a copy of the GNU General Public License along with ELLE.
  * If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 package es.udc.cartolab.gvsig.elle;
 
 import com.iver.andami.PluginServices;
@@ -22,7 +22,11 @@ import com.iver.andami.preferences.IPreference;
 import com.iver.andami.preferences.IPreferenceExtension;
 import com.iver.cit.gvsig.About;
 import com.iver.cit.gvsig.AddLayer;
+import com.iver.cit.gvsig.ProjectExtension;
 import com.iver.cit.gvsig.gui.panels.FPanelAbout;
+import com.iver.cit.gvsig.project.Project;
+import com.iver.cit.gvsig.project.ProjectFactory;
+import com.iver.cit.gvsig.project.documents.view.ProjectView;
 import com.iver.cit.gvsig.project.documents.view.gui.View;
 
 import es.udc.cartolab.gvsig.elle.gui.EllePreferencesPage;
@@ -32,49 +36,66 @@ import es.udc.cartolab.gvsig.users.utils.DBSession;
 
 public class LoadMapExtension extends Extension implements IPreferenceExtension {
 
-	public static EllePreferencesPage ellePreferencesPage = new EllePreferencesPage();
+    public static EllePreferencesPage ellePreferencesPage = new EllePreferencesPage();
 
-	public void execute(String actionCommand) {
-		LoadMapWizard wizard = new LoadMapWizard((View) PluginServices.getMDIManager().getActiveWindow());
-		wizard.open();
+    public void execute(String actionCommand) {
+	ProjectExtension pExt = (ProjectExtension) PluginServices
+		.getExtension(ProjectExtension.class);
+	Project project = pExt.getProject();
+
+	// creating view and add it to the project
+	if (!(PluginServices.getMDIManager().getActiveWindow() instanceof View)) {
+	    ProjectView doc = ProjectFactory.createView(null);
+	    doc.setName("ELLE View");
+	    doc.setProject(project, 0);
+	    project.addDocument(doc);
+	    // Opening view
+	    View elleView = new View();
+	    elleView.initialize();
+	    elleView.setModel(doc);
+	    elleView.getWindowInfo().setMaximizable(true);
+	    elleView.getWindowInfo().setMaximized(true);
+	    PluginServices.getMDIManager().addWindow(elleView);
 	}
+	LoadMapWizard wizard = new LoadMapWizard((View) PluginServices
+		.getMDIManager().getActiveWindow());
+	wizard.open();
+    }
 
-	public void initialize() {
-		About about=(About)PluginServices.getExtension(About.class);
-		FPanelAbout panelAbout=about.getAboutPanel();
-		java.net.URL aboutURL = this.getClass().getResource("/about.htm");
-		panelAbout.addAboutUrl("ELLE", aboutURL);
+    public void initialize() {
+	About about = (About) PluginServices.getExtension(About.class);
+	FPanelAbout panelAbout = about.getAboutPanel();
+	java.net.URL aboutURL = this.getClass().getResource("/about.htm");
+	panelAbout.addAboutUrl("ELLE", aboutURL);
 
-		//		carga la pestaña en añadir capa
-		AddLayer.addWizard(ElleWizard.class);
+	// carga la pestaña en añadir capa
+	AddLayer.addWizard(ElleWizard.class);
 
-		//icons
-		registerIcons();
-	}
+	// icons
+	registerIcons();
+    }
 
-	protected void registerIcons() {
+    protected void registerIcons() {
 
-		PluginServices.getIconTheme().registerDefault(
-				"load-map",
-				this.getClass().getClassLoader().getResource("images/mapacargar.png")
-			);
-	}
+	PluginServices.getIconTheme().registerDefault(
+		"load-map",
+		this.getClass().getClassLoader().getResource(
+			"images/mapacargar.png"));
+    }
 
-	public boolean isEnabled() {
-		return PluginServices.getMDIManager().getActiveWindow() instanceof View;
-	}
+    public boolean isEnabled() {
+	return PluginServices.getMDIManager().getActiveWindow() instanceof View;
+    }
 
-	public boolean isVisible() {
-		DBSession dbs = DBSession.getCurrentSession();
-		return dbs!=null;
-	}
+    public boolean isVisible() {
+	DBSession dbs = DBSession.getCurrentSession();
+	return dbs != null;
+    }
 
-	public IPreference[] getPreferencesPages() {
-		IPreference[] preferences=new IPreference[1];
-		preferences[0]=ellePreferencesPage;
-		return preferences;
-	}
-
-
+    public IPreference[] getPreferencesPages() {
+	IPreference[] preferences = new IPreference[1];
+	preferences[0] = ellePreferencesPage;
+	return preferences;
+    }
 
 }
