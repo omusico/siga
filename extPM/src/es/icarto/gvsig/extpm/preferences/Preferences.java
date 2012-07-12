f8cdf5dcaa6d997d4b1107e1bb0947dd772bcb7a
psanxiao <psanxiao@icarto.es>
2012-07-12 13:25:39 +0200

    extPM: Changed PreferencesPage by Preferences

---
+++ extPM/src/es/icarto/gvsig/extpm/preferences/Preferences.java
@@ -0,0 +1,16 @@
+package es.icarto.gvsig.extpm.preferences;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+import com.iver.andami.preferences.AbstractPreferencePage;
+import com.iver.andami.preferences.StoreException;
+
+public class Preferences {
+    
+    // ormlite
+    public static final String XML_ORMLITE_RELATIVE_PATH = "data/extpm.xml";
+    
+    public static final String PM_LAYER_NAME = "PM";
+
+}
