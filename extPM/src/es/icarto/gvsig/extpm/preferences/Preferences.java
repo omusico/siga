876ca2d5d12f1ae96b0c906e0998ff5adeb85989
psanxiao <psanxiao@icarto.es>
2012-08-09 13:34:25 +0200

    extPM: Changed PM layer name in Preferences

--- extPM/src/es/icarto/gvsig/extpm/preferences/Preferences.java
+++ extPM/src/es/icarto/gvsig/extpm/preferences/Preferences.java
@@ -1,17 +1,12 @@
 package es.icarto.gvsig.extpm.preferences;
 
-import javax.swing.ImageIcon;
-import javax.swing.JPanel;
-
-import com.iver.andami.preferences.AbstractPreferencePage;
-import com.iver.andami.preferences.StoreException;
 
 public class Preferences {
-    
+
     // ormlite
     public static final String XML_ORMLITE_RELATIVE_PATH = "data/extpm.xml";
-    
+
     public static final String PM_SCHEMA = "audasa_pm";
-    public static final String PM_LAYER_NAME = "PM";
+    public static final String PM_LAYER_NAME = "Policia_Margenes";
 
 }
