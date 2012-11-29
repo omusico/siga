ada4f9f168488044bc9e435ee00a4274b6406d6b
psanxiao <psanxiao@icarto.es>
2012-11-29 11:49:34 +0100

    extGIA: Launch form depending on layer name

--- extGIA/src/es/icarto/gvsig/extgia/FormLauncherExtension.java
+++ extGIA/src/es/icarto/gvsig/extgia/FormLauncherExtension.java
@@ -6,6 +6,7 @@ import com.iver.andami.PluginServices;
 import com.iver.andami.plugins.Extension;
 import com.iver.cit.gvsig.fmap.layers.FLyrVect;
 
+import es.icarto.gvsig.extgia.forms.isletas.IsletasForm;
 import es.icarto.gvsig.extgia.forms.taludes.TaludesForm;
 import es.icarto.gvsig.extgia.preferences.Preferences;
 import es.icarto.gvsig.navtableforms.ormlite.ORMLite;
@@ -13,26 +14,38 @@ import es.icarto.gvsig.navtableforms.utils.TOCLayerManager;
 
 public class FormLauncherExtension extends Extension {
 
+    public enum elements {
+	Taludes, Isletas;
+    }
+
     private FLyrVect layer;
 
     @Override
     public void execute(String actionCommand) {
 	// TODO. check if layer and tables are correctly loaded
 
-	if (actionCommand.equals("form_launcher")) {
-	    new Foo().execute();
-	    actionCommand = "taludes";
-	}
-
 	this.layer = getLayerFromTOC(actionCommand);
 	if (this.layer != null) {
-	    final TaludesForm form = new TaludesForm(this.layer);
-	    if (form.init()) {
-		PluginServices.getMDIManager().addWindow(form);
+	    String layerName = layer.getName();
+
+	    switch (elements.valueOf(layerName)) {
+	    case Taludes:
+		final TaludesForm taludesForm = new TaludesForm(this.layer);
+		if (taludesForm.init()) {
+		    PluginServices.getMDIManager().addCentredWindow(taludesForm);
+		}
+		break;
+	    case Isletas:
+		final IsletasForm isletasForm = new IsletasForm(this.layer);
+		if (isletasForm.init()) {
+		    PluginServices.getMDIManager().addCentredWindow(isletasForm);
+		}
+		break;
 	    }
+
 	} else {
 	    JOptionPane.showMessageDialog(null, "La capa " + actionCommand
-		    + " no est√° cargada en el TOC", "Capa no encontrada",
+		    + " no est· cargada en el TOC", "Capa no encontrada",
 		    JOptionPane.ERROR_MESSAGE);
 	}
     }
@@ -41,7 +54,7 @@ public class FormLauncherExtension extends Extension {
 	final String layerName = ORMLite
 		.getDataBaseObject(
 			Preferences.getPreferences().getXMLFilePath())
-		.getTable(actionCommand).getTableName();
+			.getTable(actionCommand).getTableName();
 	final TOCLayerManager toc = new TOCLayerManager();
 	return toc.getLayerByName(layerName);
     }
@@ -50,7 +63,7 @@ public class FormLauncherExtension extends Extension {
 	PluginServices.getIconTheme().registerDefault(
 		"extgia-openform",
 		this.getClass().getClassLoader()
-			.getResource("images/extgia-openform.png"));
+		.getResource("images/extgia-openform.png"));
     }
 
     @Override
