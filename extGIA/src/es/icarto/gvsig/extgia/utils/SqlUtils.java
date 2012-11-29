fafe1104f0f793dad49d53d1dab53c196e663975
psanxiao <psanxiao@icarto.es>
2012-11-29 16:52:53 +0100

    extGIA: Added function to create embebed JTables from database to SqlUtils

--- extGIA/src/es/icarto/gvsig/extgia/utils/SqlUtils.java
+++ extGIA/src/es/icarto/gvsig/extgia/utils/SqlUtils.java
@@ -3,8 +3,16 @@ package es.icarto.gvsig.extgia.utils;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.util.ArrayList;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import com.hardcode.gdbms.engine.values.Value;
+import com.hardcode.gdbms.engine.values.ValueFactory;
 
 import es.icarto.gvsig.navtableforms.ormlite.domain.KeyValue;
 import es.udc.cartolab.gvsig.users.utils.DBSession;
@@ -27,6 +35,43 @@ public class SqlUtils {
 	} catch (SQLException e) {
 	    e.printStackTrace();
 	    return new ArrayList<KeyValue>();
+	}
+    }
+
+    public static void createEmbebedTableFromDB (JTable embebedTableWidget, String schema,
+	    String tablename, String idField, String idValue) {
+	ArrayList<String> columnsName = new ArrayList<String>();
+	DefaultTableModel tableModel = new DefaultTableModel();
+	PreparedStatement statement;
+	String query = "SELECT * FROM " + schema + "." + tablename + " WHERE " + idField + " = '" +
+		idValue + "';";
+	try {
+	    statement = DBSession.getCurrentSession().getJavaConnection().prepareStatement(query);
+	    statement.execute();
+	    ResultSet rs = statement.getResultSet();
+	    ResultSetMetaData rsMetaData = rs.getMetaData();
+	    // Columns Name
+	    for (int i=0; i<rsMetaData.getColumnCount(); i++) {
+		columnsName.add(rsMetaData.getColumnName(i+1));
+	    }
+	    for (String columnName : columnsName) {
+		tableModel.addColumn(columnName);
+	    }
+	    embebedTableWidget.setModel(tableModel);
+	    // Values
+	    Value[] tableValues = new Value[rsMetaData.getColumnCount()];
+	    while (rs.next()) {
+		for (int i=0; i<rsMetaData.getColumnCount(); i++) {
+		    tableValues[i] =
+			    ValueFactory.createValueByType(rs.getString(i+1),
+				    rsMetaData.getColumnType(i+1));
+		}
+		tableModel.addRow(tableValues);
+	    }
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	} catch (ParseException e) {
+	    e.printStackTrace();
 	}
     }
 
