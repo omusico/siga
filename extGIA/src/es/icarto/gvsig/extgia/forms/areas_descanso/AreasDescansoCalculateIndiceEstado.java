3cec33c7739444f060e20e1442ee88f8910b87b5
psanxiao <psanxiao@icarto.es>
2012-12-17 13:47:15 +0100

    extGIA: Minor fix on calculate index in areas_descanso

--- extGIA/src/es/icarto/gvsig/extgia/forms/areas_descanso/AreasDescansoCalculateIndiceEstado.java
+++ extGIA/src/es/icarto/gvsig/extgia/forms/areas_descanso/AreasDescansoCalculateIndiceEstado.java
@@ -24,12 +24,12 @@ public class AreasDescansoCalculateIndiceEstado extends CalculateReconocimientoI
 	float value = 0;
 
 	String strA = ((KeyValue) ((JComboBox) operatorComponents
-		.get(DBFieldNames.AREA_SERVICIO_A)).getSelectedItem())
+		.get(DBFieldNames.AREA_DESCANSO_A)).getSelectedItem())
 		.getKey();
 	String strB = ((KeyValue) ((JComboBox) operatorComponents
-		.get(DBFieldNames.AREA_SERVICIO_B)).getSelectedItem()).getKey();
+		.get(DBFieldNames.AREA_DESCANSO_B)).getSelectedItem()).getKey();
 	String strC = ((KeyValue) ((JComboBox) operatorComponents
-		.get(DBFieldNames.AREA_SERVICIO_C)).getSelectedItem()).getKey();
+		.get(DBFieldNames.AREA_DESCANSO_C)).getSelectedItem()).getKey();
 
 	value += Integer.parseInt(strA) * 0.33;
 	value += Integer.parseInt(strB) * 0.33;
