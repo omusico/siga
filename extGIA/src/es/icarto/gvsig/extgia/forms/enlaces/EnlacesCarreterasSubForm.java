dbe1b78c78130a674b7c18c39ad09e4108beef84
Andrés Maneiro <amaneiro@icarto.es>
2012-12-19 12:02:04 +0100

    extGIA: Enlaces subforms, added validation and domains

--- extGIA/src/es/icarto/gvsig/extgia/forms/enlaces/EnlacesCarreterasSubForm.java
+++ extGIA/src/es/icarto/gvsig/extgia/forms/enlaces/EnlacesCarreterasSubForm.java
@@ -17,7 +17,8 @@ public class EnlacesCarreterasSubForm extends AbstractSubForm {
 
     @Override
     public String getXMLPath() {
-	return this.getClass().getClassLoader().getResource("rules/extgia.xml")
+	return this.getClass().getClassLoader()
+		.getResource("rules/enlaces_carreteras_metadata.xml")
 		.getPath();
     }
 
