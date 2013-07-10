975b99922c12be5edff1a109103cc43b13f5d00a
psanxiao <psanxiao@icarto.es>
2013-07-10 17:07:16 +0200

    extGIA: Added fieldNames for caracteristicas reports

--- extGIA/src/es/icarto/gvsig/extgia/consultas/ConsultasFieldNames.java
+++ extGIA/src/es/icarto/gvsig/extgia/consultas/ConsultasFieldNames.java
@@ -5,8 +5,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import es.icarto.gvsig.extgia.consultas.caracteristicas.AreasDescansoCaracteristicasReport;
+import es.icarto.gvsig.extgia.consultas.caracteristicas.AreasServicioCaracteristicasReport;
 import es.icarto.gvsig.extgia.consultas.caracteristicas.BarreraRigidaCaracteristicasReport;
+import es.icarto.gvsig.extgia.consultas.caracteristicas.EnlacesCaracteristicasReport;
 import es.icarto.gvsig.extgia.consultas.caracteristicas.IsletasCaracteristicasReport;
+import es.icarto.gvsig.extgia.consultas.caracteristicas.JuntasCaracteristicasReport;
+import es.icarto.gvsig.extgia.consultas.caracteristicas.PasosMedianaCaracteristicasReport;
+import es.icarto.gvsig.extgia.consultas.caracteristicas.VallaCierreCaracteristicasReport;
 import es.icarto.gvsig.extgia.preferences.DBFieldNames;
 import es.udc.cartolab.gvsig.users.utils.DBSession;
 
@@ -42,19 +47,27 @@ public class ConsultasFieldNames {
 	    return "id_barrera_rigida, tipo_via, nombre_via, pk_inicial, pk_final, obstaculo_protegido" +
 	    ", longitud, codigo, tipo, metodo_constructivo, perfil, observaciones";
 	case Areas_Servicio:
-	    break;
+	    return "nombre, tipo_via, nombre_via, municipio, pk, fecha_puesta_servicio, sup_total," +
+	    "sup_pavimentada, aceras, bordillos, zona_siega, zona_ajardinada, riego," +
+	    "cafeteria_rest_bar, aparcamiento_camion_bus, area_picnic, fuentes_potables, observaciones";
 	case Areas_Descanso:
 	    return "nombre, tipo_via, nombre_via, municipio, pk, fecha_puesta_servicio, sup_total," +
 	    "sup_pavimentada, aceras, bordillos, zona_siega, zona_ajardinada, riego," +
 	    "aparcamiento_camion_bus, area_picnic, fuentes_potables, observaciones";
+	case Enlaces:
+	    return "nombre, tipo_via, nombre_via, municipio, pk, n_salida, tipo_enlace, alumbrado," +
+	    "observaciones";
 	case Juntas:
-	    break;
+	    return "tramo, tipo_via, nombre_via, pk, numero_junta, ancho, modulo, elemento," +
+	    "codigo_elemento, descripcion, observaciones";
 	case Pasos_Mediana:
-	    break;
+	    return "tramo, tipo_via, nombre_via, pk, longitud, numero_postes, cierre, longitud_cierre," +
+	    "cuneta_entubada, observaciones";
 	case Senhalizacion_Vertical:
 	    break;
 	case Valla_Cierre:
-	    break;
+	    return "pk_inicial, pk_final, tipo_valla, longitud, altura, n_panhos, n_puertas," +
+	    "n_postes_simples, n_postes_tripode, pastor_electrico, observaciones";
 	case Firme:
 	    break;
 	}
@@ -73,17 +86,24 @@ public class ConsultasFieldNames {
 	    new BarreraRigidaCaracteristicasReport(element[1], outputFile, rs, filters);
 	    break;
 	case Areas_Servicio:
+	    new AreasServicioCaracteristicasReport(element[1], outputFile, rs, filters);
 	    break;
 	case Areas_Descanso:
 	    new AreasDescansoCaracteristicasReport(element[1], outputFile, rs, filters);
 	    break;
+	case Enlaces:
+	    new EnlacesCaracteristicasReport(element[1], outputFile, rs, filters);
+	    break;
 	case Juntas:
+	    new JuntasCaracteristicasReport(element[1], outputFile, rs, filters);
 	    break;
 	case Pasos_Mediana:
+	    new PasosMedianaCaracteristicasReport(element[1], outputFile, rs, filters);
 	    break;
 	case Senhalizacion_Vertical:
 	    break;
 	case Valla_Cierre:
+	    new VallaCierreCaracteristicasReport(element[1], outputFile, rs, filters);
 	    break;
 	case Firme:
 	    break;
