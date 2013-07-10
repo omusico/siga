4413ea8b450629c76707dae94a78c85b120f2aa2
psanxiao <psanxiao@icarto.es>
2013-07-10 17:01:50 +0200

    extGIA: Added EnlacesCaracteristicasReport

---
+++ extGIA/src/es/icarto/gvsig/extgia/consultas/caracteristicas/EnlacesCaracteristicasReport.java
@@ -0,0 +1,67 @@
+package es.icarto.gvsig.extgia.consultas.caracteristicas;
+
+import java.sql.ResultSet;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Rectangle;
+
+import es.icarto.gvsig.extgia.consultas.Report;
+
+public class EnlacesCaracteristicasReport extends Report {
+
+    public EnlacesCaracteristicasReport(String element, String fileName,
+	    ResultSet resultMap, String[] filters) {
+	super(element, fileName, resultMap, filters);
+	// TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected String getTitle() {
+	return "Listado de Características";
+    }
+
+    @Override
+    protected Rectangle setPageSize() {
+	return PageSize.A4.rotate();
+    }
+
+    @Override
+    protected String[] getColumnNames() {
+	String[] columnNames = {
+		"Nombre",
+		"Tipo Vía",
+		"Nombre Vía",
+		"Municipio",
+		"PK",
+		"Número Salida",
+		"Tipo Enlace",
+		"Alumbrado",
+		"Observaciones"
+	};
+	return columnNames;
+    }
+
+    @Override
+    protected float[] getColumnsWidth(int columnCount) {
+	float[] columnsWidth = new float[columnCount];
+
+	columnsWidth[0] = 60f;
+	columnsWidth[1] = 60f;
+	columnsWidth[2] = 60f;
+	columnsWidth[3] = 60f;
+	columnsWidth[4] = 60f;
+	columnsWidth[5] = 60f;
+	columnsWidth[6] = 60f;
+	columnsWidth[7] = 60f;
+	columnsWidth[8] = 60f;
+
+	return columnsWidth;
+    }
+
+    @Override
+    protected void writeDatesRange(Document document, String[] filters) {
+
+    }
+
+}
