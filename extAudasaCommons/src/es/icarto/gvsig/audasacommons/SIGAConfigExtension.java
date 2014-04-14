f37d574d9f3156902e22409645f644a1a29aca90
Francisco Puga <fpuga@cartolab.es>
2014-04-15 00:18:34 +0200

    extAudasaCommons: ELLE Configuration
    Configures the Tab name and the maps to show in the elle add layer wizard

--- extAudasaCommons/src/es/icarto/gvsig/audasacommons/SIGAConfigExtension.java
+++ extAudasaCommons/src/es/icarto/gvsig/audasacommons/SIGAConfigExtension.java
@@ -1,5 +1,9 @@
 package es.icarto.gvsig.audasacommons;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import com.iver.andami.PluginServices;
 import com.iver.andami.plugins.Extension;
 import com.iver.andami.preferences.IPreference;
@@ -7,7 +11,10 @@ import com.iver.andami.preferences.IPreferenceExtension;
 import com.iver.cit.gvsig.About;
 import com.iver.cit.gvsig.gui.panels.FPanelAbout;
 
-public class MockExtension extends Extension implements IPreferenceExtension {
+import es.udc.cartolab.gvsig.elle.ConfigExtension;
+import es.udc.cartolab.gvsig.elle.utils.MapFilter;
+
+public class SIGAConfigExtension extends Extension implements IPreferenceExtension {
 
     @Override
     public void initialize() {
@@ -16,7 +23,26 @@ public class MockExtension extends Extension implements IPreferenceExtension {
 	java.net.URL aboutURL = this.getClass().getResource("/about.htm");
 	panelAbout.addAboutUrl("AUDASA", aboutURL);
     }
-
+    
+    @Override
+    public void postInitialize() {
+	ConfigExtension configExt = (ConfigExtension) PluginServices.getExtension(ConfigExtension.class);
+	configExt.setWizardTitle("AUDASA");
+	configExt.setMapFilter(new MapFilter() {
+	    @Override
+	    public String[] filter(String[] maps) {
+		List<String> mapsToShow = new ArrayList<String>();
+		    for (int i=0; i<maps.length; i++) {
+			if (maps[i].startsWith("BDD_")) {
+			    mapsToShow.add(maps[i]);
+			}
+		    }
+		    Collections.sort(mapsToShow);
+		    return mapsToShow.toArray(new String[0]);
+	    }
+	});
+    }
+    
     @Override
     public void execute(String actionCommand) {
     }
