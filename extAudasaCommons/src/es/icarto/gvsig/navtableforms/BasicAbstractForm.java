4a165627adf664e732cfa19d6a4391c9f908777e
Francisco Puga <fpuga@icarto.es>
2014-10-17 20:06:58 +0200

    extAudasaCommons: Adds a sorter button to all forms

--- extAudasaCommons/src/es/icarto/gvsig/navtableforms/BasicAbstractForm.java
+++ extAudasaCommons/src/es/icarto/gvsig/navtableforms/BasicAbstractForm.java
@@ -1,11 +1,27 @@
 package es.icarto.gvsig.navtableforms;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.RowSorter.SortKey;
+
+import com.hardcode.gdbms.driver.exceptions.ReadDriverException;
 import com.iver.andami.PluginServices;
 import com.iver.cit.gvsig.fmap.layers.FLyrVect;
+import com.iver.cit.gvsig.fmap.layers.SelectableDataSource;
 import com.jeta.forms.components.panel.FormPanel;
 import com.jeta.forms.gui.common.FormException;
+
+import es.icarto.gvsig.commons.gui.OkCancelPanel;
+import es.icarto.gvsig.commons.queries.Utils;
+import es.icarto.gvsig.commons.utils.Field;
+import es.udc.cartolab.gvsig.navtable.contextualmenu.ChooseSortFieldDialog;
 
 @SuppressWarnings("serial")
 public abstract class BasicAbstractForm extends AbstractForm {
@@ -13,6 +29,42 @@ public abstract class BasicAbstractForm extends AbstractForm {
     public BasicAbstractForm(FLyrVect layer) {
 	super(layer);
 	setWindowTitle(PluginServices.getText(this, getBasicName()));
+	addSorterButton();
+    }
+
+    private void addSorterButton() {
+	java.net.URL imgURL = getClass().getClassLoader().getResource("sort.png");
+	JButton jButton = new JButton(new ImageIcon(imgURL));
+	
+	jButton.addActionListener(new ActionListener() {
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+		URL resource = BasicAbstractForm.this.getClass().getClassLoader().getResource(
+			"columns.properties");
+		List<Field> fields = Utils.getFields(resource.getPath(),
+			getSchema(), getBasicName());
+
+		ChooseSortFieldDialog dialog = new ChooseSortFieldDialog(fields);
+
+		if (dialog.open().equals(OkCancelPanel.OK_ACTION_COMMAND)) {
+		    List<Field> sortedFields = dialog.getFields();
+		    List<SortKey> sortKeys = new ArrayList<SortKey>();
+		    SelectableDataSource sds = getRecordset();
+		    for (Field field : sortedFields) {
+			try {
+			    int fieldIdx = sds.getFieldIndexByName(field
+				    .getKey());
+			    sortKeys.add(new SortKey(fieldIdx, field
+				    .getSortOrder()));
+			} catch (ReadDriverException e1) {
+			    logger.error(e1.getStackTrace(), e1);
+			}
+		    }
+		    setSortKeys(sortKeys);
+		}
+	    }
+	});
+	getActionsToolBar().add(jButton);
     }
 
     @Override
@@ -36,5 +88,7 @@ public abstract class BasicAbstractForm extends AbstractForm {
     }
 
     protected abstract String getBasicName();
+
+    protected abstract String getSchema();
 
 }
