9bdd2e49bf454943b4e9dd4feef73d325c81b982
Francisco Puga <fpuga@icarto.es>
2015-09-11 21:17:21 +0200

    extPM: Clean up
    * Removes unused images
    * Removes unused libraries
    * FormPMExtensions extends from AbstractExtension
    * Fixed some translations

--- extPM/src/es/icarto/gvsig/extpm/FormPMExtension.java
+++ extPM/src/es/icarto/gvsig/extpm/FormPMExtension.java
@@ -1,16 +1,15 @@
 package es.icarto.gvsig.extpm;
 
 import com.iver.andami.PluginServices;
-import com.iver.andami.plugins.Extension;
-import com.iver.andami.ui.mdiManager.IWindow;
 import com.iver.cit.gvsig.fmap.layers.FLyrVect;
 import com.iver.cit.gvsig.project.documents.view.gui.View;
 
+import es.icarto.gvsig.audasacommons.AbstractExtension;
 import es.icarto.gvsig.extpm.forms.FormPM;
 import es.icarto.gvsig.navtableforms.utils.TOCLayerManager;
 import es.udc.cartolab.gvsig.users.utils.DBSession;
 
-public class FormPMExtension extends Extension {
+public class FormPMExtension extends AbstractExtension {
 
     @Override
     public void execute(String actionCommand) {
@@ -22,22 +21,14 @@ public class FormPMExtension extends Extension {
     }
 
     @Override
-    public void initialize() {
-    }
-
-    @Override
     public boolean isEnabled() {
-	if ((DBSession.getCurrentSession() != null) && hasView()
+	View view = getView();
+	if ((DBSession.getCurrentSession() != null) && view != null
 		&& isLayerLoaded(FormPM.TOCNAME)) {
 	    return true;
 	} else {
 	    return false;
 	}
-    }
-
-    @Override
-    public boolean isVisible() {
-	return true;
     }
 
     private FLyrVect getPMLayer() {
@@ -52,13 +43,5 @@ public class FormPMExtension extends Extension {
 	    return false;
 	}
 	return true;
-    }
-
-    private boolean hasView() {
-	IWindow window = PluginServices.getMDIManager().getActiveWindow();
-	if (window instanceof View) {
-	    return true;
-	}
-	return false;
     }
 }
