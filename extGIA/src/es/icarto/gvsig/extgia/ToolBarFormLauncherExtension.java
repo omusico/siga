a3f932494a755f38600a0703cb1894b81ee2478b
Francisco Puga <fpuga@icarto.es>
2015-10-07 20:52:30 +0200

    extGIA: ToolBarFormLauncherExtension inherits from AbstractExtension
    * The name of the icon is chaged
    * Some strings in the config are translated
    * Also, is changed how the enability of the layer is computed. Now the
      button is enabled when there is a layer in the TOC active that has
      a form associated

--- extGIA/src/es/icarto/gvsig/extgia/ToolBarFormLauncherExtension.java
+++ extGIA/src/es/icarto/gvsig/extgia/ToolBarFormLauncherExtension.java
@@ -1,21 +1,25 @@
 package es.icarto.gvsig.extgia;
 
-import com.iver.andami.PluginServices;
-import com.iver.andami.plugins.Extension;
-import com.iver.andami.ui.mdiManager.IWindow;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.iver.cit.gvsig.fmap.layers.FLyrVect;
 import com.iver.cit.gvsig.project.documents.view.gui.View;
 
 import es.icarto.gvsig.extgia.forms.LaunchGIAForms;
 import es.icarto.gvsig.extgia.preferences.DBFieldNames;
+import es.icarto.gvsig.extgia.preferences.DBFieldNames.Elements;
 import es.icarto.gvsig.navtableforms.utils.TOCLayerManager;
+import es.icarto.gvsig.siga.AbstractExtension;
 import es.udc.cartolab.gvsig.users.utils.DBSession;
 
-public class ToolBarFormLauncherExtension extends Extension {
+public class ToolBarFormLauncherExtension extends AbstractExtension {
 
-    @Override
-    public void initialize() {
-	registerIcons();
+    private final static List<String> elementList = new ArrayList<String>();
+    static {
+	for (Elements e : DBFieldNames.Elements.values()) {
+	    elementList.add(e.name());
+	}
     }
 
     @Override
@@ -23,63 +27,27 @@ public class ToolBarFormLauncherExtension extends Extension {
 	TOCLayerManager toc = new TOCLayerManager();
 	FLyrVect[] layers = toc.getActiveLayers();
 	for (FLyrVect layer : layers) {
-	    if (layer != null) {
-		LaunchGIAForms.callFormDependingOfLayer(layer.getName(), false);
-	    }
+	    LaunchGIAForms.callFormDependingOfLayer(layer.getName(), false);
 	}
-    }
-
-    protected void registerIcons() {
-	PluginServices.getIconTheme().registerDefault(
-		"extgia-openform",
-		this.getClass().getClassLoader()
-		.getResource("images/extgia-openform.png"));
     }
 
     @Override
     public boolean isEnabled() {
-	if ((DBSession.getCurrentSession() != null) &&
-		hasView() && isAnyLayerLoaded()) {
-	    return true;
-	} else {
-	    return false;
-	}
+	return DBSession.isActive() && activeInventoryLayer();
     }
 
-    private boolean hasView() {
-	IWindow window = PluginServices.getMDIManager().getActiveWindow();
-	if(window instanceof View) {
-	    return true;
-	}
-	return false;
-    }
-
-    private boolean isAnyLayerLoaded() {
-	IWindow iWindow = PluginServices.getMDIManager().getActiveWindow();
-	if (!(iWindow instanceof View)) {
+    private boolean activeInventoryLayer() {
+	View view = getView();
+	if (view == null) {
 	    return false;
 	}
-	for (int i=0; i<DBFieldNames.Elements.values().length; i++) {
-	    if (isLayerLoaded(DBFieldNames.Elements.values()[i].toString())) {
+	TOCLayerManager toc = new TOCLayerManager(view.getMapControl());
+	FLyrVect[] layers = toc.getActiveLayers();
+	for (FLyrVect l : layers) {
+	    if (elementList.contains(l.getName())) {
 		return true;
 	    }
 	}
 	return false;
-
     }
-
-    private boolean isLayerLoaded(String layerName) {
-	TOCLayerManager toc = new TOCLayerManager();
-	FLyrVect layer = toc.getLayerByName(layerName);
-	if(layer == null) {
-	    return false;
-	}
-	return true;
-    }
-
-    @Override
-    public boolean isVisible() {
-	return true;
-    }
-
 }
